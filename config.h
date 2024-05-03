d6317e57aeb3399788831b5c61aadef1e373657a
Toby Hansen <tobyhansen@outlook.com>
2024-05-02 20:38:56 -0700

    Added second font and scrolback

--- config.h
+++ config.h
@@ -6,6 +6,13 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "SpaceMono Nerd Font:pixelsize=20:antialias=true:autohint=true";
+
+/* Spare fonts */
+static char *font2[] = {
+	"emoji:pixelsize=20:antialias=true:autohint=true", 
+
+};
+
 static int borderpx = 2;
 
 /*
@@ -201,6 +208,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
